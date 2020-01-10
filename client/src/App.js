078cd76900ea3114df452b7040439717b48c1752
André Pardal <andrecspardal@gmail.com>
2020-01-10 19:28:51 +0000

    Mint & deposit & withdraw

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import BankContract from "./contracts/bank.json";
 import NFTContract from "./contracts/NFToken.json";
+import ERC721Contract from "./contracts/ERC721.json";
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
 
 class App extends Component {
-  state = { tokenId: 0, web3: null, accounts: null, contract: null };
+  state = {
+    tokenId: 0,
+    web3: null,
+    accounts: null,
+    contract: null,
+    bankBalance: 0,
+    ownerBalance: 0
+  };
 
   componentDidMount = async () => {
     try {
@@ -29,12 +37,16 @@ class App extends Component {
         NFTContract.abi,
         NFTdeployedNetwork && NFTdeployedNetwork.address,
       );
+      const ERC721Instance = new web3.eth.Contract(
+        ERC721Contract.abi,
+        NFTdeployedNetwork && NFTdeployedNetwork.address,
+      );
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      const newState = { web3, accounts, BankInstance, NFTInstance }
+      const newState = { web3, accounts, BankInstance, ERC721Instance, NFTInstance }
       console.log(newState)
-      this.setState(newState, this.runExample);
+      this.setState(newState);
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -44,52 +56,78 @@ class App extends Component {
     }
   };
 
-  runExample = async () => {
-    const { accounts, BankInstance, NFTInstance } = this.state;
-    const owner = accounts[0]
-    
-    // const mintResponse = await NFTInstance.methods.mintToken(owner, 0).send({ from: owner})
-    // debugger
-    
-    // const safeTransferTokenResponse = await NFTInstance.methods.safeTransferFrom(owner, BankInstance._address, 0).send({ from: owner})
-
-    // const checkNewOwnerResponse = await NFTInstance.methods.ownerOf(0).send({ from: owner})
-
-    // this.setState({ : checkOwnerResponse });
-  };
+  getNftBalanceOf = async (address) => {
+    const { ERC721Instance } = this.state
+    let result = null;
+    try {
+      result = await ERC721Instance.methods.balanceOf(address).call()
+      console.log({address, balanceOf: result})
+    } catch (error) {
+      console.log(error)
+    }
+    return result
+  }
   
   handleTokenIdInput = (event) => {
     this.setState({ tokenId: Number(event.target.value)})
   }
 
   mintTokenTransfer = async () => {
-    const { NFTInstance, BankInstance, accounts, tokenId } = this.state
+    const { NFTInstance, accounts, tokenId } = this.state
     const owner = accounts[0]
     try {
       const mintResponse = await NFTInstance.methods.mintToken(owner, tokenId).send({ from: owner, gas: 5000000 })
       console.log({mintResponse})
-      const safeTransferResponse = await NFTInstance.methods.safeTransferFrom(owner, BankInstance._address, tokenId).send({ from: owner, gas: 5000000 })
-      console.log({safeTransferResponse})
+      // const safeTransferResponse = await ERC721Instance.methods.safeTransferFrom(owner, BankInstance._address, tokenId).send({ from: owner, gas: 5000000 })
+      // console.log({safeTransferResponse})
+    } catch (error) {
+      console.log(error)
+    }
+    this.updateTokenCount()
+  }
+
+  checkBank = async () => {
+    const { ERC721Instance, BankInstance, accounts, tokenId } = this.state
+    const owner = accounts[0]
+    try {
+      const beforeSafeTransfer = await BankInstance.methods._tokenOwner(ERC721Instance._address, tokenId).call({ from: owner, gas: 5000000 })
+      console.log({beforeSafeTransfer})
+      const bankResponse = await BankInstance.methods.safeTransferFrom(ERC721Instance._address, owner, owner, tokenId, '0x0a').send({ from: owner, gas: 5000000 })
+      console.log({bankResponse})
+      const afterSafeTransfer = await BankInstance.methods._tokenOwner(ERC721Instance._address, tokenId).call({ from: owner, gas: 5000000 })
+      console.log({afterSafeTransfer})
+      // const response = await BankInstance.methods._tokenOwner(ERC721Instance._address, Number(tokenId)).call({ from: owner, gas: 5000000 })
+      // console.log(response)
     } catch (error) {
       console.log(error)
     }
   }
 
-  checkBank = async () => {
-    const { NFTInstance, BankInstance, accounts, tokenId } = this.state
+  withdraw = async () => {
+    const { ERC721Instance, BankInstance, accounts, tokenId } = this.state
     const owner = accounts[0]
-    try {
-      const beforeSafeTransfer = await BankInstance.methods._tokenOwner(NFTInstance._address, tokenId).call({ from: owner, gas: 5000000 })
-      console.log({beforeSafeTransfer})
-      const bankResponse = await BankInstance.methods.safeTransferFrom(NFTInstance._address, owner, owner, tokenId, '0x0a').send({ from: owner, gas: 5000000 })
-      console.log({bankResponse})
-      const afterSafeTransfer = await BankInstance.methods._tokenOwner(NFTInstance._address, tokenId).call({ from: owner, gas: 5000000 })
-      console.log({afterSafeTransfer})
-      // const response = await BankInstance.methods._tokenOwner(NFTInstance._address, Number(tokenId)).call({ from: owner, gas: 5000000 })
-      // console.log(response)
-    } catch (error) {
-      console.log(error)
-    }
+    const bankResponse = await BankInstance.methods.safeTransferFrom(ERC721Instance._address, owner, owner, tokenId, '0x0a').send({ from: owner, gas: 5000000 })
+    console.log({bankResponse})
+
+    this.updateTokenCount()
+  }
+
+  deposit = async () => {
+    const { ERC721Instance, BankInstance, accounts, tokenId } = this.state
+    const owner = accounts[0]
+    const safeTransferResponse = await ERC721Instance.methods.safeTransferFrom(owner, BankInstance._address, tokenId).send({ from: owner, gas: 5000000 })
+    console.log({safeTransferResponse})
+
+    this.updateTokenCount()
+  }
+
+  updateTokenCount = async () => {
+    const { BankInstance, accounts } = this.state
+    const owner = accounts[0]
+    const bankBalance = await this.getNftBalanceOf(BankInstance._address)
+    const ownerBalance = await this.getNftBalanceOf(owner)
+    console.log({bankBalance, ownerBalance})
+    this.setState({bankBalance, ownerBalance})
   }
 
   render() {
@@ -107,10 +145,20 @@ class App extends Component {
         <p>
           Try changing the value and click mint. (for now check console log on DevTools)
         </p>
+        <p>
+          Owner address {this.state.accounts[0]} NFT Balance: {this.state.ownerBalance}
+        </p>
+        <p>
+          NFT Contract address {this.state.ERC721Instance._address}
+        </p>
         <label>Token id</label>
         <input onChange={this.handleTokenIdInput} value={this.state.tokenId}/>
         <button onClick={this.mintTokenTransfer}>Mint Token</button>
-        <button onClick={this.checkBank}>checkBank</button>
+        <p>
+          Bank Contract address {this.state.BankInstance._address} NFT Balance: {this.state.bankBalance}
+        </p>
+        <button onClick={this.deposit}>Deposit</button>
+        <button onClick={this.withdraw}>Withdraw</button>
       </div>
     );
   }
