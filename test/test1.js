a7071d1240ef49a0d874f9e64fccd130323f4f73
Jack Watters <squawfreeskier@gmail.com>
2020-01-29 18:53:43 -0800

    updated test cases

--- test/test1.js
+++ test/test1.js
@@ -11,51 +11,53 @@ const MyNFTContract = contract.fromArtifact("NFToken");
 let mybank;
 let myNFT;
 
-describe("MyContract", function() {
-  before(async function() {
-    this.timeout(50000);
-    mybank = await MyBankContract.new({ from: owner });
-    myNFT = await MyNFTContract.new({ from: owner });
-  });
+describe("Rocket Nft Integration Tests", function() {
+  describe("Standard Deposit and Withdraw flow", function() {
+    before(async function() {
+      this.timeout(50000);
+      mybank = await MyBankContract.new({ from: owner });
+      myNFT = await MyNFTContract.new({ from: owner });
+    });
 
-  it("deployer is owner, mintToken", async function() {
-    await myNFT.mintToken(owner, 0);
-    expect(await myNFT.ownerOf(0)).to.equal(owner);
-  });
-
-  it("should be able to transfer token to bank", async function() {
-    await myNFT.safeTransferFrom(owner, mybank.address, 0, { from: owner });
-    expect(await myNFT.ownerOf(0)).to.equal(mybank.address);
-  });
-
-  it("contract bank is owner", async function() {
-    expect(await myNFT.ownerOf(0)).to.equal(mybank.address);
-  });
-
-  it("contract bank understands who is owner internally", async function() {
-    expect(await mybank.ownerOf(myNFT.address, 0)).to.equal(owner);
-  });
-
-  it("original owner is owner in bank", async function() {
-    expect(await mybank.ownerOf(myNFT.address, 0)).to.equal(owner);
-  });
-
-  it("should be able to transfer from bank to original contract", async function() {
-    await mybank.safeTransferFrom(myNFT.address, owner, owner, 0, "0x0a", {
-      from: owner
-    }),
+    it("deployer is owner, mintToken", async function() {
+      await myNFT.mintToken(owner, 0);
       expect(await myNFT.ownerOf(0)).to.equal(owner);
-  });
+    });
 
-  it("original owner is owner", async function() {
-    expect(await myNFT.ownerOf(0)).to.equal(owner);
-  });
+    it("should be able to transfer token to bank", async function() {
+      await myNFT.safeTransferFrom(owner, mybank.address, 0, { from: owner });
+      expect(await myNFT.ownerOf(0)).to.equal(mybank.address);
+    });
 
-  it("original owner is not owner after he has transferred it", async function() {
-    expectRevert(
-      mybank.ownerOf(myNFT.address, 0),
-      "ERC721: owner query for nonexistent token"
-    );
+    it("contract bank is owner", async function() {
+      expect(await myNFT.ownerOf(0)).to.equal(mybank.address);
+    });
+
+    it("contract bank understands who is owner internally", async function() {
+      expect(await mybank.ownerOf(myNFT.address, 0)).to.equal(owner);
+    });
+
+    it("original owner is owner in bank", async function() {
+      expect(await mybank.ownerOf(myNFT.address, 0)).to.equal(owner);
+    });
+
+    it("should be able to transfer from bank to original contract", async function() {
+      await mybank.safeTransferFrom(myNFT.address, owner, owner, 0, "0x0a", {
+        from: owner
+      }),
+        expect(await myNFT.ownerOf(0)).to.equal(owner);
+    });
+
+    it("original owner is owner", async function() {
+      expect(await myNFT.ownerOf(0)).to.equal(owner);
+    });
+
+    it("original owner is not owner after he has transferred it", async function() {
+      expectRevert(
+        mybank.ownerOf(myNFT.address, 0),
+        "ERC721: owner query for nonexistent token"
+      );
+    });
   });
   describe("seize collateral scenario", function() {
     before(async function() {
@@ -86,6 +88,8 @@ describe("MyContract", function() {
     });
 
     it("admin seize collateral", async function() {
+      expect(await myNFT.ownerOf(0)).to.equal(mybank.address);
+      expect(await mybank.ownerOf(myNFT.address, 0)).to.equal(receiver);
       await mybank.adminCollateralize(myNFT.address, owner, 0, "0x0a", {
         from: owner
       });
@@ -100,7 +104,8 @@ describe("MyContract", function() {
         expect(await myNFT.ownerOf(0)).to.equal(owner);
     });
   });
-  describe("failing test", function() {
+
+  describe("Admin Locks Token, Token Depositor Withdraw Fails", function() {
     before(async function() {
       this.timeout(50000);
       mybank = await MyBankContract.new({ from: owner });
@@ -129,18 +134,25 @@ describe("MyContract", function() {
       expect(await mybank.isAdminLocked(myNFT.address, 0)).to.equal(true);
     });
 
-    // it("withdraw should fail", async function() {
-    //   await mybank.safeTransferFrom(
-    //     myNFT.address,
-    //     receiver,
-    //     receiver,
-    //     0,
-    //     "0x0a",
-    //     {
-    //       from: owner
-    //     }
-    //   ),
-    //     expect(await myNFT.ownerOf(0)).to.equal(receiver);
-    // });
+    it("withdraw should fail", async function() {
+      expectRevert(
+        mybank.safeTransferFrom(myNFT.address, receiver, receiver, 0, "0x0a", {
+          from: receiver
+        }),
+        "administrator lock"
+      );
+    });
+  });
+
+  describe("Ownable functions openzepplin", function() {
+    it("Owner Is Owner", async function() {
+      expect(await mybank.isOwner({ from: owner })).to.equal(true);
+    });
+    it("reciever is not owner", async function() {
+      expect(await mybank.isOwner({ from: receiver })).to.equal(false);
+    });
+    it("Owner owns contract", async function() {
+      expect(await mybank.owner()).to.equal(owner);
+    });
   });
 });
